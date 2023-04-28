fb59620d3e0624c569d9db61d58d61aef8d91ce8
maarcosfeitosa <maarcosfeitosa@gmail.com>
2023-04-28 19:58:16 -0300

    teste

--- app/templates/__init__.py
+++ app/templates/__init__.py
@@ -1,11 +1,24 @@
+import pyodbc
+from app.controllers import app
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from controllers import default
+from sqlalchemy import create_engine
+import pandas as pd
+
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
 
 
-asd
 
+SERVER = 'DESKTOP-8J8RVEB\SQLEXPRESS'
+DATABASE = 'GanhosUber'
+DRIVER = 'SQL Server'
+USERNAME = ''
+PASSWORD = ''
+DATABASE_CONNECTION = f'mssql://{USERNAME}:{PASSWORD}@{SERVER}/{DATABASE}?driver={DRIVER}'
+engine = create_engine(DATABASE_CONNECTION)
+connection = engine.connect()
+data = pd.read_sql_query(f"SELECT * FROM Usuario", connection)
+print(data)
 
