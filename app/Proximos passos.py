e6bfa5978e6dc3da56b5bf5bc72344a61aa9a030
maarcosfeitosa <maarcosfeitosa@gmail.com>
2023-04-29 15:14:12 -0300

    Apaguei arquivos que nao usaria no flask Flask esta funcionando e interagindo com BD

--- app/Proximos passos.py
+++ app/Proximos passos.py
@@ -1,15 +1,9 @@
 """"
 O proximo passo:
 
-Colocar uma coluna para puxar os dados da tabela
-depois disso vou arrumar as entradas dos valores para nao correr riscos
-de digitar caracter onde precisa ser numero, etc etc
+Ele nao esta verificando as informaçções de cara label, se digitar sem virgula ele da erro...
+Se deixar label vazia ele nao manda para o BD, ele volta para criar de novo, tem que preencher tudo
 
-Preciso entender mais sobre o class pra ver se ele eh algo que sera util e funcional
-
-
-depois de inserir e mostrar resultados vou colocar opcao tambem de conferir se a data
-digitada ja se encontra na tabela e se quero alterar
 
 
 
